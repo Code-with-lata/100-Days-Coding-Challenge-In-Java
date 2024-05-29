c265884b252143bf2ff0f80f8130e8030780a257
Lata <prajapatilata01@gmail.com>
2024-05-29 13:53:51 +0530

    add

--- Day-1/TypeConversion.java
+++ Day-1/TypeConversion.java
@@ -2,6 +2,6 @@ public class TypeConversion {
     public static void main(String[] args) {
         int a = 10;
         long b = a;
-        System.out.println(b); //int ->long is aloud but long -> int is not aloud  
+        System.out.println(b); //int ->long is not aloud but long -> int is aloud.  
     }
 }
---
+++ Day-1/TypePromotion.java
@@ -0,0 +1,10 @@
+public class TypePromotion {
+    public static void main(String[] args) {
+        char a ='a';
+        char b = 'b';
+        int sum = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+    }
+}
