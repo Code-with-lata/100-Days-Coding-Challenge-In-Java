a8a2e68badc7aee0e566295c90370d3688027a47
Lata <prajapatilata01@gmail.com>
2024-05-29 13:25:25 +0530

    add

---
+++ Day-1/TypeConversion.java
@@ -0,0 +1,7 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = a;
+        System.out.println(b); //int ->long is aloud but long -> int is not aloud  
+    }
+}
