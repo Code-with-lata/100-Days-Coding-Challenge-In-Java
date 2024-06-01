f4b99db0f97583f2751ef007435ee18b801ccd5d
Lata <prajapatilata01@gmail.com>
2024-06-01 15:30:43 +0530

    add

--- Day-6/TopRightTrianglPattern.java
+++ Day-6/TopRightTrianglPattern.java
@@ -6,11 +6,11 @@ public class TopRightTrianglPattern {
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(i+j >=n+1){
-                    System.out.print("");
+                if(j-i >=0){
+                    System.out.print("*");
                 }
                 else{
-                    System.out.print("*");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
