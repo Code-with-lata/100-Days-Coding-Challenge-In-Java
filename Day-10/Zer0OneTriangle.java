32b513418a8e79e4e45ab38601423559d4cf99b3
Lata <prajapatilata01@gmail.com>
2024-06-02 22:13:25 +0530

    add

--- Day-10/Zer0OneTriangle.java
+++ Day-10/Zer0OneTriangle.java
@@ -1,11 +1,6 @@
 public class Zer0OneTriangle {
     public static void zero_One_triangle(int n){
-        // int total_number_of_line = n;
-        // int stars = 1;
-        // int spaces = n-1;
-        // int current_number_of_line = 1;
-        // while ((current_number_of_line <= total_number_of_line)) {
-        //print star
+        
            for(int i=1;i<=n;i++){
                 for (int j = 1; j <=i; j++){  
                     if((i+j) % 2 ==0){
@@ -17,17 +12,6 @@ public class Zer0OneTriangle {
                 }
                 System.out.println();
             }
-           //print spaces
-        //    for(int i=1;i<=spaces;i++){
-        //        System.out.print(" ");
-        //    }
-
-        //prepare for the next line
-           
-        //    stars++;
-        //    spaces--;
-           
-        //    current_number_of_line++;
         
     }
     public static void main(String[] args) {
