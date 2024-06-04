85b1f2f61b1a3eaf45c83945e5bff76774bdc6db
Lata <prajapatilata01@gmail.com>
2024-06-04 21:38:25 +0530

    add

--- Day-12/printAllSubarrays.java
+++ Day-12/printAllSubarrays.java
@@ -1,14 +1,17 @@
 public class printAllSubarrays {
     public static void printSubArray(int arr[]){
+        int totalArray = 0;
         for(int i=0;i<arr.length;i++){
             for(int j=i;j<arr.length;j++){
                  for(int k=i;k<=j;k++){
                     System.out.print(arr[k]+ " ");
                  }
+                 totalArray++;
                  System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total Sub Array : " +totalArray);
     }
     public static void main(String[] args) {
         int arr[] = {9,4,7,3,10,12};
