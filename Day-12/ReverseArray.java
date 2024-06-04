be1e556c7219e44ee29e64012f1f2ec6ef31dab0
Lata <prajapatilata01@gmail.com>
2024-06-04 20:50:34 +0530

    add

---
+++ Day-12/ReverseArray.java
@@ -0,0 +1,27 @@
+public class ReverseArray {
+    public static void ReverseArray(int arr[]){
+       int first = 0;  
+       int last=arr.length-1; 
+       
+       while (first< last){
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+
+            first++;
+            last--;
+       }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        ReverseArray(arr);
+        for(int i=0;i<=arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        
+
+        
+    }
+    
+}
