0dfd4fc188adbc7808b882a255b9bfa1e9b4d392
Lata <prajapatilata01@gmail.com>
2024-06-05 21:17:43 +0530

    add

--- Day-13/PrintCharactorPattern.java
+++ Day-13/PrintCharactorPattern.java
@@ -1,5 +1,15 @@
 public class PrintCharactorPattern {
-    
+    public static void PrintCharactorPattern(int n){
+        char ch = 'A';
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         
     }
