3c1877c0c31b69c111b66128e047b3640969b6c1
Lata <prajapatilata01@gmail.com>
2024-06-06 21:22:47 +0530

    add

--- Day-14/MaxSubArrayUsingBruteForce.java
+++ Day-14/MaxSubArrayUsingBruteForce.java
@@ -4,6 +4,7 @@ public class MaxSubArrayUsingBruteForce {
         int maxSum = Integer.MIN_VALUE;
         for(int i=0;i<=arr.length;i++){
             for(int j=i;j<arr.length;j++){
+                currSum = 0;
                 for(int k=i;k<=j;k++){
                     currSum += arr[k];
                 }
@@ -13,8 +14,9 @@ public class MaxSubArrayUsingBruteForce {
                 }
             }
         }
+        
         System.out.println("max Sum = " +maxSum);
-
+        
     }
 
     public static void main(String[] args) {
