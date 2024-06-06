61b096732fac55746e3d047a07e66aa5c97448ac
Lata <prajapatilata01@gmail.com>
2024-06-06 22:03:53 +0530

    add

---
+++ Day-14/MaxSubArrayUsingPrefixSum.java
@@ -0,0 +1,31 @@
+public class MaxSubArrayUsingPrefixSum {
+    public static void maxSubArray(int arr[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int [arr.length];
+        
+        prefix[0] = arr[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i] = prefix[i-1]+arr[i];
+        }
+
+        for(int i=0;i<arr.length;i++){
+            //int start = i;
+            for(int j=i;j<=arr.length;j++){
+                //int end = j
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+
+                if(maxSum < currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("max sum : " +maxSum);
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        maxSubArray(arr);
+        
+    }
+    
+}
