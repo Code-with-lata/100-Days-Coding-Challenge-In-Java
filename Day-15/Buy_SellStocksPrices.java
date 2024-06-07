3bf33f4e910ff6543b45076b21818f552fed29a2
Lata <prajapatilata01@gmail.com>
2024-06-07 23:53:08 +0530

    add

--- Day-15/Buy_SellStocksPrices.java
+++ Day-15/Buy_SellStocksPrices.java
@@ -1,6 +1,22 @@
 public class Buy_SellStocksPrices {
+    public static int buyAndSellStocks(int prices[]){
+        int buyPrices = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0;i<prices.length;i++){
+            if(buyPrices < prices[i]){
+                int profit = prices[i] - buyPrices;//today's profit
+                maxprofit = Math.max(maxprofit,profit);
+            }
+            else{
+                buyPrices = prices[i];
+            }
+        }
+        return maxprofit;
+    }
     public static void main(String[] args) {
         int prices[] = {7,1,5,3,6,4};
+        System.out.println("Profit of the stocks is : " +buyAndSellStocks(prices));
     }
     
 }
