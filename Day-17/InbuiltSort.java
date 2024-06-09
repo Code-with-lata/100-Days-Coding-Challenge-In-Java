7a1b752c24dce943bb6fffcd7931372033ef9868
Lata <prajapatilata01@gmail.com>
2024-06-09 21:54:29 +0530

    add

--- Day-17/InbuiltSort.java
+++ Day-17/InbuiltSort.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class InbuiltSort {
     public static void main(String[] args) {
-        
+        int arr[] = {5,4,1,3,2};
+        Arrays.sort(arr);
+        System.out.println(arr);
     }
     
 }
