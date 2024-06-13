4b06417b807c4bf38ea887d4adeb347ba8c1bacd
Lata <prajapatilata01@gmail.com>
2024-06-13 20:57:56 +0530

    add

---
+++ Day-20/String.java
@@ -0,0 +1,28 @@
+//count how many times lowercase vowels occured in a string entered by the user
+
+import java.util.*;
+
+public class String {
+    public static int findlowerCaseVowelCount(String str){
+        int len = str.length();
+        int vowelCount = 0;
+
+        for(int i=0;i<len;i++){
+            char ch = str.charAt(i);
+
+            if(Character.isLowerCase(ch)==true){
+                if(ch =='a' || ch == 'e' || ch =='i' || ch == 'o' || ch == 'u'){
+                    vowelCount++;
+                }
+            }
+        }
+        return vowelCount;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int vowelCount = findlowerCaseVowelCount(str);
+
+        System.out.println();
+    }
+}
