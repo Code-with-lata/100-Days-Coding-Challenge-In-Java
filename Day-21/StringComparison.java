3c0ff6717cc672f716e00654f5bd2a7acb23f6df
Lata <prajapatilata01@gmail.com>
2024-06-16 20:57:08 +0530

    add

--- Day-21/StringComparison.java
+++ Day-21/StringComparison.java
@@ -1,10 +1,22 @@
 public class StringComparison {
     public static void stringComparison(String s1, String s2, String s3){
-        if(s1 equals(s2))
+        if(s1 == s2){//use (s1.equals(s2) for coparison)
+            System.out.println("yes1");
+        }
+        else{
+            System.out.println("No1");
+        }
+        if(s1 == s3){
+            System.out.println("Yes2");
+        }
+        else{
+            System.out.println("No2");
+        }
     }
     public static void main(String[] args) {
         String s1 = "xyz";
         String s2 = "xyz";
-        String s3 = "xyz";
+        String s3 = new String("xyz");
+        stringComparison(s1, s2, s3);
     }
 }
