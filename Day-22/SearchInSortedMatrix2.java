7fa68c981201d874b5acabe37264d2a5f0a72b11
Lata <prajapatilata01@gmail.com>
2024-06-19 22:02:47 +0530

    add

--- Day-22/SearchInSortedMatrix2.java
+++ Day-22/SearchInSortedMatrix2.java
@@ -1,18 +1,18 @@
 public class SearchInSortedMatrix2 {
     public static boolean topStairCaseSearch(int matrix[][], int key){
-         int row = matrix.length-1,col= 0;
+        int row = matrix.length-1,col= 0;
 
-         while (row >= 0 && col < matrix.length) {
+        while (row >= 0 && col < matrix.length) {
             if(matrix[row][col] == key){
                 System.out.println("found key at (" + row + "," + col + ")");
                 return true;
 
             }
             else if(matrix[row][col] < key){
-                row--;
+                col++;
             }
             else{
-                col++;
+                row--;
             }
          }
         System.out.println("key not found");
@@ -21,7 +21,7 @@ public class SearchInSortedMatrix2 {
     public static void main(String[] args) {
         int matrix[][] = {{10,20,30,40},
                           {15,25,35,45},
-                          {29,29,37,48},
+                          {27,29,37,48},
                           {32,33,39,50}};
         
         int key = 30;
