3bf6bb40fd2a1aead9df018f195624cac0ea8aaa
Lata <prajapatilata01@gmail.com>
2024-06-20 19:11:15 +0530

    add

--- Day-23/findIndices.java
+++ Day-23/findIndices.java
@@ -1,4 +1,4 @@
-//for a given integer array size N.you hava to findall the occurrences
+//for a given integer array size N.you hava to find all the occurrences
 //(indices) of the given element(key)
 
 public class findIndices {
@@ -14,7 +14,7 @@ public class findIndices {
     public static void main(String[] args) {
         int arr[] = {3,2,4,5,6,2,7,2,2};
         int key = 2;
-        findIndices(arr, key, key);
+        findIndices(arr, 0, key);
     }
     
 }
