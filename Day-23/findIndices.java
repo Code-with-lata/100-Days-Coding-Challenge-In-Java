13b52e7af96d4223303089145a71f4353a0bfe0e
Lata <prajapatilata01@gmail.com>
2024-06-20 18:53:18 +0530

    add

--- Day-23/findIndices.java
+++ Day-23/findIndices.java
@@ -2,12 +2,19 @@
 //(indices) of the given element(key)
 
 public class findIndices {
-    public static void findIndices(int arr[],int key){
+    public static void findIndices(int arr[],int index,int key){
         
+        if(index < arr.length){
+            if(arr[index] == key){
+                System.out.println(index + " ");
+            }
+            findIndices(arr,index+1, key);
+        }
     }
     public static void main(String[] args) {
         int arr[] = {3,2,4,5,6,2,7,2,2};
         int key = 2;
+        System.out.println(findIndices(arr,0,key));
     }
     
 }
