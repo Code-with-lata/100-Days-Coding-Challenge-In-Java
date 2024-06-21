024c637317757db298a1358699cd1886c42124c2
Lata <prajapatilata01@gmail.com>
2024-06-21 21:31:44 +0530

    add

---
+++ Day-24/factorialNo.java
@@ -0,0 +1,14 @@
+public class factorialNo {
+    public static int factorial_No(int n){
+        if(n == 1){
+            return 1;
+        }
+        int fact 1
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial_No(n));
+    }
+
+    
+}
