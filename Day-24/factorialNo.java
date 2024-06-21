8a7fd936a23a0aa34090ad5198b3271a4de0b467
Lata <prajapatilata01@gmail.com>
2024-06-21 21:33:42 +0530

    add

--- Day-24/factorialNo.java
+++ Day-24/factorialNo.java
@@ -3,11 +3,13 @@ public class factorialNo {
         if(n == 1){
             return 1;
         }
-        int fact 1
+        int fact1 = factorial_No(n-1);
+        int fact = n* fact1;
+        return fact;
     }
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(factorial_No(n));
+        System.out.println("factorial of" + n + "is" + factorial_No(n));
     }
 
     
