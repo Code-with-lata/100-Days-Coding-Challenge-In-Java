0fca9434b9a1f1e6a6da2d926163d31081d4466b
Lata <prajapatilata01@gmail.com>
2024-06-29 15:59:55 +0530

    add

---
+++ Day-26/DiagonalSum_in2D_Array.java
@@ -0,0 +1,32 @@
+public class DiagonalSum_in2D_Array {
+    public static int diagonalSum(int matrix[][]){
+        int sum = 0;
+        //using brout force
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j==matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        for(int i=0;i<matrix.length;i++){
+            //pD
+            sum += matrix[i][i];
+
+            //SD
+            sum +
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},
+                          {5,6,7,8},
+                          {9,10,11,12},
+                          {13,14,15,16}};
+        System.out.println("diagonal sum is : " +diagonalSum(matrix));                     
+    }
+}
