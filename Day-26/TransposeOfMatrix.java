f5d6dd2f46ab5eb4a4dde2374dd3c50214688417
Lata <prajapatilata01@gmail.com>
2024-06-29 16:39:24 +0530

    add

--- Day-26/TransposeOfMatrix.java
+++ Day-26/TransposeOfMatrix.java
@@ -6,9 +6,10 @@ public class TransposeOfMatrix {
                 transpose[j][i] = matrix[i][j];
             }
         }
+        System.out.println("transpose of the matrix is : ");
         for(int i=0;i<transpose.length;i++){
             for(int j=0;j<transpose[0].length;j++){
-                System.out.print(transpose[i][j]+ " ");
+                System.out.print( transpose[i][j]+ " ");
             }
             System.out.println();
         }
@@ -17,7 +18,8 @@ public class TransposeOfMatrix {
     public static void main(String[] args) {
         int matrix[][] ={{2,3,7},
                          {5,6,7}};
-        //print original matrix                 
+        //print original matrix 
+        System.out.println("original matrix is :");                
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 System.out.print(matrix[i][j]+ " ");
