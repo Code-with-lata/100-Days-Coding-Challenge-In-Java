cb0524ce4e8e58e3a6f5380fc3450527864bc655
Lata <prajapatilata01@gmail.com>
2024-06-30 23:05:06 +0530

    add

--- Day-27/FindLargestString.java
+++ Day-27/FindLargestString.java
@@ -1,12 +1,16 @@
 public class FindLargestString {
-    public static void FindLargestString(String fruits[]){
+    public static void findLargestString(String fruits[]){
         String largest = fruits[0];
         for(int i=0;i<fruits.length;i++){
-            
+            if(largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            }
         }
+        System.out.println(largest);
     }
     public static void main(String[] args) {
         String fruits[] = {"apple", "mango", "banana"};
+        findLargestString(fruits);
     }
     
 }
