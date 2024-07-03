68a12b0222114c085be12e85d9b55ca6b30d8cfc
Lata <prajapatilata01@gmail.com>
2024-07-03 21:48:26 +0530

    add

---
+++ Day-30/Odd_EvenInBitWiseOperator.java
@@ -0,0 +1,15 @@
+public class Odd_EvenInBitWiseOperator {
+    public static void OddEven(int num){
+        int bitmask = 1;
+        if(num & bitmask == 0){
+            System.out.println("num is even");
+        }
+        else{
+            System.out.println("num is odd");
+        }
+    }
+    public static void main(String[] args) {
+        OddEven(5);
+        
+    }
+}
