3c7eaa5aa98746c8ca971b14159bf93716df4469
Lata <prajapatilata01@gmail.com>
2024-07-03 22:08:16 +0530

    add

---
+++ Day-30/Get_ith_bit.java
@@ -0,0 +1,12 @@
+public class Get_ith_bit {
+    public static int getIthBit(int num ,int i){
+        int bitmask = (1<<i);
+        if((num & bitmask) == 0){
+            return 0;
+        }
+        else{return 1;}
+    } 
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 2));
+    }
+}
