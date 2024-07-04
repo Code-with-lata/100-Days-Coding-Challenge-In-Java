28410ab70663a16b0fe4f81c3fc127eca01463ed
Lata <prajapatilata01@gmail.com>
2024-07-04 22:36:54 +0530

    add

--- Day-31/ClearRangeOfBit.java
+++ Day-31/ClearRangeOfBit.java
@@ -1,3 +1,11 @@
 public class ClearRangeOfBit {
-    
+    public static int changeRangeOfBit(int n,int i,int j ){
+        int a =((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int BitMask = a|b;
+        return n & BitMask;
+    }    
+    public static void main(String[] args) {
+        System.out.println(changeRangeOfBit(10, 2, 4));
+    }
 }
