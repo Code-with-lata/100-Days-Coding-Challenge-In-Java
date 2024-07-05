0ea068099f1f6cd647d294f47aabc33a1308da0a
Lata <prajapatilata01@gmail.com>
2024-07-05 16:54:39 +0530

    add

---
+++ Day-32/AccessModifier.java
@@ -0,0 +1,18 @@
+public class AccessModifier {
+    public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount();
+        myAccount.username = "Lata";
+        System.out.println(myAccount.username);
+        myAccount.setPassword("sfgyyybferevb");
+        // System.out.println(myAccount.password);
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+}
