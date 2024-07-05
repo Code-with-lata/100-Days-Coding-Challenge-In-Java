9b66ef5800a0b46f930e83664710e84abf0fc7c6
Lata <prajapatilata01@gmail.com>
2024-07-05 22:35:49 +0530

    add

--- Day-32/ConstructorType.java
+++ Day-32/ConstructorType.java
@@ -1,3 +1,23 @@
 public class ConstructorType {
-    
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Lata");
+        Student s3 = new Student(123);
+    }
+}
+class Student {
+    String name;
+    int roll;
+
+    //Non-Parametrized Constructor
+    Student() {
+        System.out.println("Constructor is call....");
+    }
+    //Parametrized Constructor
+    Student(String name) {
+        this.name = name;
+    }
+    Student(int roll) {
+        this.roll = roll;
+    }
 }
