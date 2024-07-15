46b43a78e9c0d247adfb7fd69b2dfab6dee02559
Lata <prajapatilata01@gmail.com>
2024-07-15 11:21:13 +0530

    add

--- Day-40/QuickSort.java
+++ Day-40/QuickSort.java
@@ -1,7 +1,7 @@
 public class QuickSort {
     public static void printArr(int arr[]){
         for(int i =0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
