9c8c5b0ff1ccf44f9c4641942225f4ec55c17d2b
Lata <prajapatilata01@gmail.com>
2024-07-23 22:58:01 +0530

    add

---
+++ Day-44/N_Queen.java
@@ -0,0 +1,32 @@
+public class N_Queen {
+    public static void nQueens(char board[][], int row){
+        if(row == board.length){
+            printBoard(board);
+        }
+        for(int j=0;j<board.length;j++){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        }
+    }
+    public static void printBoard(char board[][]){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n =2;
+        char board[][] = new char[n][n];
+        // initialize
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = '.';
+
+            }
+        }
+    }
+    
+}
